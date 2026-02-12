5937bf3b976b4aff6a8c177fefb6879ea8b1d5a0
Dilara Sarach <dilara.sarach@abv.bg>
2026-02-12 12:27:40 +0100

    dummy endpoint added to main.py

--- app/main.py
+++ app/main.py
@@ -6,10 +6,18 @@ from app.routers import dataset, lookup
 def create_app() -> FastAPI:
     app = FastAPI(title="taxSun API")
     add_cors(app)
-    #app.include_router(dataset.router, prefix="/process")
-    #app.include_router(lookup.router, prefix="/lookup")
+
+    @app.get("/")
+    def root():
+        return {"status": "ok", "message": "taxSun backend alive"}
+
+    @app.get("/health")
+    def health():
+        return {"healthy": True}
+
     app.include_router(dataset.router)
     app.include_router(lookup.router)
+
     return app
 
 app = create_app()
